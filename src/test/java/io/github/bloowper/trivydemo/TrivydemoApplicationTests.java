1449039f0cc10e34077b7c9d186f72bc6ffd3285
Tomasz Orchowski <tomasz.orchowski@codibly.com>
2023-04-17 18:45:52 +0200

    initial commit

---
+++ src/test/java/io/github/bloowper/trivydemo/TrivydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bloowper.trivydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrivydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
