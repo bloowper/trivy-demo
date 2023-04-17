25bf4cdf607567abb4be5a4650154656842631e4
Tomasz Orchowski <tomasz.orchowski@codibly.com>
2023-04-17 23:00:33 +0200

    Trivy ci/cd changes

--- src/main/java/io/github/bloowper/trivydemo/TrivydemoApplication.java
+++ src/main/java/io/github/bloowper/trivydemo/TrivydemoApplication.java
@@ -1,13 +1,17 @@
 package io.github.bloowper.trivydemo;
 
+import com.google.common.collect.Comparators;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class TrivydemoApplication {
 
 	public static void main(String[] args) {
-
+		List<Integer> integers = List.of(3, 4, 5);
+		Comparators.isInOrder(integers, Integer::compareTo);
 		SpringApplication.run(TrivydemoApplication.class, args);
 	}
 
